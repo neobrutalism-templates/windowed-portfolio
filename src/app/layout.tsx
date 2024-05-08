0db4c118eaf3ecd165ac4dea2618618e19e84aa3
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-05-08 02:57:16 +0200

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Archivo } from 'next/font/google'
 import './globals.css'
+import Nav from '@/components/nav'
 
-const inter = Inter({ subsets: ['latin'] })
+const archivo = Archivo({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={archivo.className}>
+        <div className="w600:grid-cols-[70px_auto] w500:grid-cols-1 grid h-[800px] max-h-[100dvh] w-[1000px] max-w-[1000px] grid-cols-[100px_auto] rounded-base shadow-[10px_10px_0_0_#000] outline outline-4 outline-black portrait:h-[100dvh]">
+          <header className="w500:hidden relative flex items-center justify-center rounded-l-base border-r-4 border-r-black bg-main portrait:rounded-none">
+            <h1 className="w600:text-[30px] w600:tracking-[2px] -rotate-90 whitespace-nowrap text-[40px] font-bold tracking-[4px] smallHeight:text-[30px] smallHeight:tracking-[2px]">
+              <span className="inline-block">JOHN DOE</span>
+            </h1>
+          </header>
+          <main className="relative flex h-[800px] max-h-[100dvh] flex-col rounded-br-base rounded-tr-base bg-bg font-semibold portrait:h-[100dvh] portrait:max-h-[100dvh] portrait:rounded-none">
+            <Nav />
+            <div className="main h-full max-h-[750px] overflow-y-auto portrait:max-h-[calc(100dvh-50px)]">
+              {children}
+            </div>
+          </main>
+        </div>
+      </body>
     </html>
   )
 }
