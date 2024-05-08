571d4568187b6e7a620d6fd8ca1182b138af5836
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-05-08 16:58:47 +0200

    styling changes

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -37,28 +37,31 @@ export default function Page() {
       {PROJECTS.map((project, id) => {
         return (
           <div
-            className="w600:px-[30px] w400:px-5 border-b-4 border-r-4 border-b-black border-r-black bg-bg p-8 py-10"
+            className="border-b-4 border-r-4 border-b-black border-r-black bg-bg p-8 py-10 w600:px-[30px] w400:px-5"
             key={id}
           >
-            <div className="w800:w-full mx-auto w-3/4">
-              <AspectRatio ratio={2 / 1}>
+            <div className="mx-auto w-3/4 w800:w-full">
+              <AspectRatio
+                className="!-bottom-[2px] rounded-base border-2 border-black shadow-base"
+                ratio={2 / 1}
+              >
                 <img
-                  className="w-full rounded-base border-2 border-black shadow-base"
+                  className="w-full rounded-base"
                   src={`${project.previewImage}`}
                   alt={project.name}
                 />
               </AspectRatio>
 
               <div className="mt-6">
-                <h2 className="w700:text-2xl w450:text-xl text-3xl font-bold">
+                <h2 className="text-3xl font-bold w700:text-2xl w450:text-xl">
                   {project.name}
                 </h2>
 
-                <p className="w450:text-base mt-5 text-lg">
+                <p className="mt-5 text-lg w450:text-base">
                   {project.description}
                 </p>
 
-                <div className="w400:text-sm mt-8 grid grid-cols-2 gap-5 text-base">
+                <div className="mt-8 grid grid-cols-2 gap-5 text-base w400:text-sm">
                   <a
                     className="cursor-pointer rounded-base border-2 border-black bg-main px-4 py-2 text-center uppercase shadow-base transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none"
                     href={project.liveLink}
